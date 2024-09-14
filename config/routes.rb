3b7fd6be38b7a26b7bf94ec1927ddf1bb764a0b2
arisa0315 <pyonsu0315@gmail.com>
2024-09-15 01:22:51 +0900

    ルーティング作成

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,9 @@
 Rails.application.routes.draw do
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+  root 'shifts#index'
 
-  # Defines the root path route ("/")
-  # root "articles#index"
+  resources :shifts, only: [:index, :new, :create, :edit, :update]
+
+  resources :users, only: [:new, :create]
+
+  resources :productions, only: [:new, :create, :edit, :update]
 end
